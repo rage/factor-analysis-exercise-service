f7b42da65a5bf030b56a34b6a82d78f159d46105
Anastasia Diseth <annadiseth@gmail.com>
2022-08-10 13:54:10 +0300

    super cool breed select dropdown with searchable feature (sorted)

--- src/components/BreedList.tsx
+++ src/components/BreedList.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable i18next/no-literal-string */
 import { css } from "@emotion/css"
+import Select from "react-select"
 
 import Breeds from "../data/breeds_fin.json"
 
@@ -9,12 +10,30 @@ interface Props {
   chosenBreed: string | null
 }
 
+interface SelectOption {
+  value: string
+  label: string
+}
+
 const BreedList: React.FC<React.PropsWithChildren<Props>> = ({
   onClick,
   disabled,
   chosenBreed,
 }) => {
-  const breeds = Breeds.Rotu
+  const breedOptions: SelectOption[] = Breeds.Rotu.map((breed) => {
+    return { value: breed, label: breed }
+  })
+  const sortedOptions = breedOptions.sort((n1, n2) => {
+    if (n1.value > n2.value) {
+      return 1
+    }
+    if (n1.value < n2.value) {
+      return -1
+    }
+    return 0
+  })
+
+  const selectedOption = { value: chosenBreed ?? "", label: chosenBreed ?? "" }
   return (
     <div
       className={css`
@@ -22,24 +41,15 @@ const BreedList: React.FC<React.PropsWithChildren<Props>> = ({
         flex-direction: column;
       `}
     >
-      <select
-        name="breeds"
-        id="breed-select"
-        onChange={(event) => onClick(event.target.value)}
-        disabled={disabled}
-        defaultValue={chosenBreed ? chosenBreed : "default"}
-      >
-        <option value="default" disabled selected>
-          Choose your breed
-        </option>
-        {breeds.map((breed, idx) => {
-          return (
-            <option key={idx} value={breed}>
-              {breed}
-            </option>
-          )
-        })}
-      </select>
+      <Select<SelectOption>
+        options={sortedOptions}
+        placeholder="Select breed"
+        isSearchable={true}
+        value={selectedOption}
+        onChange={(e) => onClick(e?.value ?? "")}
+        isDisabled={disabled}
+        maxMenuHeight={400}
+      />
     </div>
   )
 }
