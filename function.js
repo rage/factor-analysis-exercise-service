2f59ed8f93f589e27b2d5512dd4d540b7e6c8383
Anastasia Diseth <annadiseth@gmail.com>
2022-08-18 14:17:48 +0300

    try fixing cloud functions deployment

--- function.js
+++ function.js
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { https } = require("firebase-functions")
+const functions = require("firebase-functions")
 const { default: next } = require("next")
 
 const isDev = process.env.NODE_ENV !== "production"
 
 const server = next({
   dev: isDev,
-  //location of .next generated after running -> yarn build
+  //location of .next generated after running -> npm run build
   conf: { distDir: ".next" },
 })
 
 const nextjsHandle = server.getRequestHandler()
-exports.nextServer = https.onRequest((req, res) => {
+exports.nextServer = functions.region("europe-west3").https.onRequest((req, res) => {
   return server.prepare().then(() => nextjsHandle(req, res))
 })
