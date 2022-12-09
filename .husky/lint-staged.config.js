680120a9037276bbff82cdd704260159a8a65117
Anastasia Diseth <annadiseth@gmail.com>
2022-12-09 17:42:15 +0200

    update system-tests and github workflow

--- .husky/lint-staged.config.js
+++ .husky/lint-staged.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "npx tsc --noEmit",
   "*.{md,json,scss,css}": "prettier --write",
   "system-tests/src/**/*.{js,jsx,ts,tsx}": () => [
-    `./bin/check-no-test-only-in-system-tests`,
     "npx tsc -p system-tests --noEmit",
   ],
 }
