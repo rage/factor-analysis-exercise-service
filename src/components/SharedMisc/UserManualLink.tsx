0901d858f5684466a2c9d67ce1ba4b1140fcbeee
Anastasia Diseth <annadiseth@gmail.com>
2023-02-08 10:26:56 +0200

    add manual link to edit-view, some fixes

---
+++ src/components/SharedMisc/UserManualLink.tsx
@@ -0,0 +1,16 @@
+const UserManualLink = () => {
+  return (
+    <>
+      <p>{"user manual:"}</p>
+      <a
+        href="https://github.com/rage/factor-analysis-exercise-service/blob/main/docs/usermanual.md"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {"https://github.com/rage/factor-analysis-exercise-service/blob/main/docs/usermanual.md"}
+      </a>
+    </>
+  )
+}
+
+export default UserManualLink
